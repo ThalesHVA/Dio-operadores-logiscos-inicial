5ccb997ce6931712f0403ee0db3ec20df18b47a0
Thales Henrique <108342596+ThalesHVA@users.noreply.github.com>
2023-12-08 11:24:23 -0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,12 @@
+class ninja {
+    constructor(name,age,clan,ataque){
+        this.name = name;
+        this.age = age;
+        this.clan = clan;
+        this.ataque = ataque;
+    }
+}
+let chunin = new ninja("Shikamaru","16","Nara","Posseção das Sombras")
+console.log(chunin["name","age","clan"])
+console.log("seu ataque é " + chunin["ataque"])
+\\ codigo do exerciio escrevendo classes de um jogo 
